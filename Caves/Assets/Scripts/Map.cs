d95cd2e81b095c8df0a009037fd12c51a3f62b64
Sterlios <kuzmin.s.anton@gmail.com>
2022-05-02 20:47:04 +0200

    change count Cell

--- Caves/Assets/Scripts/Map.cs
+++ Caves/Assets/Scripts/Map.cs
@@ -5,7 +5,6 @@ public class Map : MonoBehaviour
 {
     [SerializeField] private Cell cellPrefab;
     private IDictionary<IntVector2,Cell> cells = new Dictionary<IntVector2, Cell>();
-    private IDictionary<IntVector2, Cell> activeCells = new Dictionary<IntVector2, Cell>();
     private int side = 20;
     private int size => Random.Range((int)Mathf.Pow(side,2)*(3/4), (int)Mathf.Pow(side, 2));
     private int cellIndex = 0;
@@ -13,30 +12,16 @@ public class Map : MonoBehaviour
     public void Generate()
     {
         Cell target = GetNextCell(new IntVector2(0,0));
-        while (activeCells.Count != 0)
+        while (cellIndex != size)
         {
             IntVector2 newDirection = Directions.ToIntVector2(Directions.RandomValue);
             target = GetNextCell(target.coordinate + newDirection);
-            if (cellIndex == size)
-            {
-                activeCells.Clear();
-            }
         }
     }
 
     private Cell GetNextCell(IntVector2 coordinate)
     {
         Cell next = cells.ContainsKey(coordinate) ? cells[coordinate] : CreateCell(coordinate);
-        if (activeCells.ContainsKey(coordinate))
-        {
-            if (activeCells.ContainsKey(coordinate + Directions.ToIntVector2(Direction.East)) &&
-                activeCells.ContainsKey(coordinate + Directions.ToIntVector2(Direction.West)) &&
-                activeCells.ContainsKey(coordinate + Directions.ToIntVector2(Direction.North)) &&
-                activeCells.ContainsKey(coordinate + Directions.ToIntVector2(Direction.South)))
-            {
-                activeCells.Remove(coordinate);
-            }
-        }
         return next;
     }
 
@@ -49,7 +34,6 @@ public class Map : MonoBehaviour
         newCell.transform.localPosition =
             new Vector3(coordinate.x, 0f, coordinate.z);
         cells.Add(coordinate,newCell);
-        activeCells.Add(coordinate, newCell);
         cellIndex++;
         return newCell;
     }
